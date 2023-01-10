42bd35b08d975dee4f1e380b78f8a45cb672fdea
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2023-01-10 22:40:53 +0530

    feat: `mobile version added`

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
 
-const navbarlinks = document.getElementsByClassName('menu-items')[0]
+const navbarlinks = document.getElementsByClassName("menu-items")[0];
 
-toggleButton.addEventListener('click', () => {
-    navbarlinks.classList.toggle('active')
-})
+toggleButton.addEventListener("click", () => {
+  navbarlinks.classList.toggle("active");
+});
+
+// const isMobile = window.matchMedia(
+//   "only screen and (max-width: 768px)"
+// ).matches;
+
+// if (isMobile) {
+//   window.location.href = "./mobile/";
+// }
